27d4b24b879956ea7231d7f11de885ea4b62ab5a
Tomasz Parada <tomaszparada1986@gmail.com>
2019-12-01 14:49:12 +0100

    [change] - jakies tam zmiany

--- src/main/java/pl/mpas/firstspringexample/controller/AddressRestController.java
+++ src/main/java/pl/mpas/firstspringexample/controller/AddressRestController.java
@@ -1,20 +1,34 @@
 package pl.mpas.firstspringexample.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.mpas.firstspringexample.domain.Address;
+import pl.mpas.firstspringexample.service.AdressService;
 
 @RestController
 @RequestMapping("/address")
 public class AddressRestController {
+
+    private AdressService addressService;
+
+    public AddressRestController(AdressService addressService) {
+        this.addressService = addressService;
+    }
 
     // /address/my-address
     @PostMapping("/my-address")
     public Address getMyAddress() {
         return new Address("Warszawa", "unknown",
                 "22-222");
+    }
+    //../address/save?
+    @GetMapping("/save")
+    public Address saveNewAddres(@RequestParam("city") String city
+            ,@RequestParam("street") String street
+            ,@RequestParam("postalCode") String postalCode){
+
+        Address toSave=new Address(city,street,postalCode);
+        return addressService.saveNewAddress(toSave);
+
     }
 }
--- src/main/java/pl/mpas/firstspringexample/domain/Address.java
+++ src/main/java/pl/mpas/firstspringexample/domain/Address.java
@@ -2,10 +2,25 @@ package pl.mpas.firstspringexample.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//Krok 1 -nadaj encje na klasa
+@Entity
 public class Address {
+    //2 Utworz klucz głowny private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String city;
     private String street;
     private String postalCode;
+
+    // 3 Konstruktor bezargumentowy
+    public Address() {
+    }
 
     public Address(String city, String street, String postalCode) {
         this.city = city;
@@ -29,7 +44,8 @@ public class Address {
     @Override
     public String toString() {
         return "Address{" +
-                "city='" + city + '\'' +
+                "id=" + id +
+                ", city='" + city + '\'' +
                 ", street='" + street + '\'' +
                 ", postalCode='" + postalCode + '\'' +
                 '}';
